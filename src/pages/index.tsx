f6e90165c6073d5a0097aa4aa726b6365b28f9a2
kimjinsoo <roorooro9933@gmail.com>
2021-05-11 16:43:49 +0900

    fetching post data on client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import NavBar from "../components/NavBar";
+import { useQuery } from "urql";
+
+const POSTS_QUERY = {
+  query: `
+query Posts {
+    posts{
+      id
+      createdAt
+      updatedAt
+      title
+    }
+  }
+`,
+};
 
 const Index = () => {
+  const [{ data }] = useQuery(POSTS_QUERY);
   return (
     <>
       <NavBar />
       <div>hello world</div>
+      <br />
+      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
     </>
   );
 };
---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,1 @@
+export const createUrqlClient = (ssrExchage: any) => {};
